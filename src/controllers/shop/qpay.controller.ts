ff7b833a0fd0c635afb6956506d6bf0a24fc7706
Aru0077 <285309697@qq.com>
2025-04-05 21:20:07 +0800

    3

--- src/controllers/shop/qpay.controller.ts
+++ src/controllers/shop/qpay.controller.ts
@@ -73,7 +73,7 @@ export const qpayController = {
                   const orderId = req.query.order_id as string;
                   const callbackData = req.body;
 
-                  logger.info('QPay回调内容', { orderId, callbackData });
+                  logger.info('QPay回调内容', callbackData);
 
                   if (!orderId) {
                         logger.error('QPay回调缺少order_id参数', { query: req.query });
